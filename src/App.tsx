52de900f69ec17d77bd8181f8a5ea7e387705283
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-06-08 17:38:06 -0400

    concluindo o rodapé

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Rotas from './routes'
 import GlobalCss from './styles'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Header />
       </div>
       <Rotas />
+      <Footer />
     </BrowserRouter>
   )
 }

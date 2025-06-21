565ab1bfbd107b0e98ec10ff62bc064c614a7ad0
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-06-20 20:11:22 -0400

    componentizando a section

---
+++ src/components/Section/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+import { Props } from '.'
+import { cores } from '../../styles'
+import { Card } from '../Product/styles'
+
+export const Container = styled.section<Omit<Props, 'title' | 'games'>>`
+  background-color: ${(props) =>
+    props.background === 'black' ? cores.preta : cores.cinza};
+  padding: 32px 0;
+
+  ${Card} {
+    background-color: ${(props) =>
+      props.background === 'black' ? cores.cinza : cores.preta};
+  }
+
+  p {
+    font-size: 14px;
+    line-height: 22px;
+    max-width: 640px;
+  }
+`
+
+export const Title = styled.h2`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 40px;
+`

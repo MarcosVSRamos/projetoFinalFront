8598476928c85b1ca3cbdae062e16fa6e35e0e99
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-07-20 11:21:52 -0400

    integrando api no checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -7,9 +7,11 @@ import boleto from '../../assets/images/boleto.png'
 import cartao from '../../assets/images/cartao.png'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
+import { usePurchaseMutation } from '../../services/api'
 
 const Checkout = () => {
   const [payWithCard, setPayWithCard] = useState(false)
+  const [purchase, { isLoading, isError, data }] = usePurchaseMutation()
 
   const form = useFormik({
     initialValues: {
@@ -71,7 +73,39 @@ const Checkout = () => {
       )
     }),
     onSubmit: (values) => {
-      console.log(values)
+      purchase({
+        billing: {
+          document: values.cpf,
+          email: values.email,
+          name: values.fullName
+        },
+        delivery: {
+          email: values.deliveryEmail
+        },
+        payment: {
+          installments: 1,
+          card: {
+            active: payWithCard,
+            code: Number(values.cardCodeSecurity),
+            name: values.cardName,
+            number: values.cardNumber,
+            owner: {
+              document: values.cpfCardOwnder,
+              name: values.cardOwnder
+            },
+            expires: {
+              month: 1,
+              year: 2025
+            }
+          }
+        },
+        products: [
+          {
+            id: 1,
+            price: 10
+          }
+        ]
+      })
     }
   })
 
@@ -330,7 +364,11 @@ const Checkout = () => {
           </div>
         </>
       </Card>
-      <Button type="button" title="Clique aqui para finalizar a compra">
+      <Button
+        onClick={form.handleSubmit}
+        type="button"
+        title="Clique aqui para finalizar a compra"
+      >
         Finalizar compra
       </Button>
     </form>
